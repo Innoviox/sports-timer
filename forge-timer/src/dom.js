76847a279df3176cdc9ea5272ea5fccd7a562960
Simon Chervenak <sarras305@gmail.com>
2020-02-07 10:54:25 -0500

    fix leaading zero issue

--- forge-timer/src/dom.js
+++ forge-timer/src/dom.js
@@ -15,11 +15,14 @@ const tick = (el, max, padding, update_time) => {
     }, update_time);
 };
 
-
+window.odometerOptions = {
+    auto: false, // Don't automatically initialize everything with class 'odometer'
+    numberLength: 2,
+};
 
 $(document).ready(() => {
     tick($("#hours"), 99, 2, 360000);
     tick($('#minutes'), 60, 2, 60000);
     tick($('#seconds'), 60, 2, 1000);
-    tick($('#ms'), 1000, 4, 1);
+    tick($('#ms'), 1000, 4, 100);
 });
