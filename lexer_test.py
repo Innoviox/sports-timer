8fcdce8c413418eecd2e9c5cdfdf3b62058e4c8a
Simon Chervenak <sarras305@gmail.com>
2020-02-09 16:18:10 -0500

    parser works

--- lexer_test.py
+++ lexer_test.py
@@ -1,17 +1,15 @@
 import re
 
 def parse_phases(s, d=0):
+    if '.' not in s:
+        return [i.split() for i in s.split("\n")]
     phases = []
     found_phases = re.split(r'\n {%d}\d*\.' % d, s)
-    print(d, len(found_phases), found_phases)
-    #input()
     for k, phase in enumerate(found_phases):
-        print('\t'*d, k, phase)
-        #input()
         n1 = phase.split("\n")[0]
         if "-" in n1:
             *_, name, _, di, t = n1.strip().split()
-            phases.append([name, ["", di, t]])
+            phases.append([name, [[di, t]]])
         else:
             name, *lines = phase.split("\n")
             lines = "\n".join(lines)
@@ -31,15 +29,54 @@ def parse(s):
     lines = "\n".join(lines)
 
     return (name, parse_phases(lines))
-    
+
+def _disp_timers(arr, n):
+    for k in arr:
+        name, phase = k
+        if '.' in name:
+            name = name.split(".")[1]
+        name = name.strip()
+        for i in phase:
+            if i[0] in ['down', 'up']:
+                print(f"{n} > {name} > {i[0]} {i[1]}")
+            else:
+                _disp_timers([i], f"{n} > {name}")
+
+def disp_timers(s):
+    n, k = parse(s)
+    _disp_timers(k, n)
 
 timer = """# My Timer
 1. 1st Phase
   1. 1st Cell
     1. Plank - down 0:15
   2. 2nd Cell
-    1. Run - up 20:00
-    2. Bike - down 30:00"""
+    up 20:00
+    down 30:00"""
 
 
-print(parse(timer))
+# print(parse(timer))
+disp_timers(timer)
+
+[
+    ['1. 1st Phase',
+     [
+         ['  1. 1st Cell',
+          [
+              ['Plank',
+               [
+                   ['down', '0:15']
+                   ]
+               ]
+              ]
+          ],
+         [' 2nd Cell',
+          [
+              ['up', '20:00'],
+              ['down', '30:00']
+              ]
+          ]
+         ]
+     ]
+    ]
+
