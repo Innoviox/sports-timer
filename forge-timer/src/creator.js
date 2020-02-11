8ed44b368062765375a1d8d1776b2cde8b3f60cb
Adam <39442291+Aurathic@users.noreply.github.com>
2020-02-11 10:50:05 -0500

    Add, remove rows from 'Create' form

--- forge-timer/src/creator.js
+++ forge-timer/src/creator.js
@@ -1,8 +1,14 @@
 let newRow = 
-`<div>
-    Hello!
+`<div class="row">
+    <input id="name"></input>
+    <input id="direction"></input>
+    <input id="length"></input>
 </div>`;
 
 const addNewRow = () => {
     $("#rows").append(newRow);
+}
+
+const deleteLastRow = () => {
+    $("#rows").children().last().remove();
 }
